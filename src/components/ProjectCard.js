95210ea94553cbae1e2656d809bd15378977e10e
Dan Zajdband <dan.zajdband@gmail.com>
2016-01-08 12:20:40 -0300

    Added project card styles.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,9 @@
 import React, { PropTypes, Component } from 'react'
 import { Link } from 'react-router'
+import styles from 'styles/components/_project-card.scss'
 
 // Constants
-const cardClassName = 'col-xs-12 col-md-3'
+const cardClassName = `col-xs-12 col-md-3 ${styles['ProjectCard__container']}`
 
 export default class ProjectCard extends Component {
   static propTypes = {
@@ -13,17 +14,20 @@ export default class ProjectCard extends Component {
   render () {
     const { project, dashboard_slug } = this.props
     return (
-      <Link
-        to={`/dashboards/${dashboard_slug}/projects/${project.id}`}
-        className={cardClassName}>
-        <div>
-          <img src={project.cover} />
-        </div>
-        <div>
-          <h3>{project.title}</h3>
-          <p>{project.description}</p>
-        </div>
-      </Link>
+      <div className={cardClassName}>
+        <Link
+          to={`/dashboards/${dashboard_slug}/projects/${project.id}`}>
+          <div style={{
+            backgroundImage: `url(${project.cover || 'http://placehold.it/300x300'})`
+          }}
+          className={styles['ProjectCard__cover']}>
+          </div>
+          <div className={styles['ProjectCard__desc']}>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+          </div>
+        </Link>
+      </div>
     )
   }
 }
