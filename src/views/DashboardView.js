7a9b733dc7438cdf6d300eaa9a402942d312b43c
Dan Zajdband <dan.zajdband@gmail.com>
2016-01-12 11:28:12 -0300

    Added empty state to dashboard.

--- src/views/DashboardView.js
+++ src/views/DashboardView.js
@@ -20,15 +20,29 @@ class DashboardView extends Component {
   render () {
     const { dashboards, params } = this.props
     const dashboard = dashboards[params.dashboard_slug]
-
     if (dashboard) {
-      return (
-        <div className='container text-center'>
-          <h1>{dashboard.title}</h1>
-          <hr />
-          {dashboard.projects && dashboard.projects.map(project => <ProjectCard dashboard_slug={dashboard.slug} project={project} key={project.id} />)}
-        </div>
-      )
+      if (dashboard.projects && dashboard.projects.length) {
+        return (
+          <div className='container text-center'>
+            <h1>{dashboard.title}</h1>
+            <hr />
+            {dashboard.projects && dashboard.projects.map(project =>
+              <ProjectCard dashboard_slug={dashboard.slug} project={project} key={project.id} />)}
+          </div>
+        )
+      } else {
+        return (
+          <div className='container text-center'>
+            <h1>{dashboard.title}</h1>
+            <hr />
+            <ProjectCard dashboard_slug={dashboard.slug} project={{
+              id: 'create',
+              title: 'Create your first project!',
+              description: 'Click here to create your first dashboard project.'}}
+              key={0} />
+          </div>
+        )
+      }
     } else {
       return (<p>Loading...</p>)
     }
